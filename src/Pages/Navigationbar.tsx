150bc7998e55173ec2302da4f00467e3387e11ef
efgalvao <solfieri@hotmail.com>
2023-04-22 14:55:59 -0300

    refactor navigationbar

--- src/Pages/Navigationbar.tsx
+++ src/Pages/Navigationbar.tsx
@@ -2,22 +2,40 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Navbar } from 'react-bootstrap';
 
-
 export const Navigationbar = () => {
-  return (<>
-    <Navbar >
+  const loggedIn = localStorage.getItem('user');
+
+  const Logged = () => {
+    return (
       <Container>
         <Link to="/">Home</Link>
         <Link to="/accounts">Accounts</Link>
         <Link to="/cards">Cards</Link>
         <Link to="/categories">Categories</Link>
         <Link to="/shortcuts">ShortCuts</Link>
-        <Link to="/signup">Signup</Link>
-        <Link to="/login">Login</Link>
         <Link to="/logout">Logout</Link>
         <Link to="/about">About</Link>
       </Container>
-    </Navbar><br />
-  </>
-  )
-}
+    );
+  };
+
+  const NotLogged = () => {
+    return (
+      <Container>
+        <Link to="/">Home</Link>
+        <Link to="/signup">Signup</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/about">About</Link>
+      </Container>
+    );
+  };
+
+  return (
+    <>
+      <Navbar>
+        {loggedIn ? <Logged /> : <NotLogged />}
+      </Navbar>
+      <br />
+    </>
+  );
+};
